640c2abc3d8b84887cbe81f10784b306524f3d61
Steven Redman <StevenRedman122@gmail.com>
2021-06-23 12:27:42 +0100

    script.js: Added playRound function. Merged getComputerChoice() function into computerPlay(). // Player can now play a single round against the cpu.Results displayed in console.

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const ROCK = "Rock";
-const PAPER = "Paper";
+const ROCK = "ROCK";
+const PAPER = "PAPER";
 const SCISSORS = "Scissors";
 
-
-computerPlay();
+const playerSelection = prompt("Make your choice: ROCK, PAPER or SCISSORS? ");
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
 
 
 function computerPlay() {
-    let choice = getComputerChoice();
-console.log(choice);
-
-}
-
-
-function getComputerChoice() {
     let random = Math.random();
 
     if (random <= 0.33) {
@@ -22,8 +16,53 @@ function getComputerChoice() {
     else if (random <= 0.66) {
         return PAPER;
     }
-    
-        return SCISSORS;
-    
+
+    return SCISSORS;
 
 }
+
+
+
+function playRound(playerSelection, computerSelection) {
+
+   
+    playerSelection = playerSelection.toUpperCase();
+    if (playerSelection === computerSelection) {
+        return `${playerSelection} and ${computerSelection} are evenly matched. It's a draw`;
+
+    } else if (playerSelection === ROCK) {
+        switch (computerSelection) {
+            case PAPER: return `${computerSelection} covers ${playerSelection}. You LOSE!!!`;
+            case SCISSORS: return `${playerSelection} crushes ${computerSelection}. You WIN!!!`;
+         
+
+
+        }
+
+    }else if (playerSelection === PAPER) {
+        switch (computerSelection) {
+            case ROCK: return `${playerSelection} covers ${computerSelection}. You WIN!!!`;
+            case SCISSORS: return `${computerSelection} cuts ${playerSelection}. You LOSE!!!`;
+
+
+        }
+
+    }else if (playerSelection === SCISSORS) {
+        switch (computerSelection) {
+            case ROCK: return `${computerSelection} crushes ${playerSelection}. You LOSE!!!`;
+            case PAPER: return `${playerSelection} cuts ${computerSelection}. You WIN!!!`;
+
+
+        }
+
+    }
+
+
+
+
+}
+
+
+    
+
+
